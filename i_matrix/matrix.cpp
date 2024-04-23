ee2210f9cf2490b04938e1847f9ece3c2296d44e
vadomerka <pradomiri@gmail.com>
2024-04-23 17:45:46 +0300

    i matrix 2

--- i_matrix/matrix.cpp
+++ i_matrix/matrix.cpp
@@ -1,2 +1,15 @@
 #include "matrix.h"
 
+Matrix::Matrix(size_t rows_num, size_t cols_num) 
+  : Matrix()
+{
+  // TODO: интуиция...
+  if (rows_num == 0 && cols_num == 0) {
+    return;
+  }
+
+  // Либо оба нули, либо оба не нули.
+  if (rows_num == 0 || cols_num == 0) {
+    // TODO: throw runtime exception
+  }
+}
--- i_matrix/matrix.h
+++ i_matrix/matrix.h
@@ -4,9 +4,26 @@
 #include <cstddef>
 
 class Matrix {
+public:
+  Matrix() 
+    : _storage(nullptr) // Выполняется ИНИЦИАЛИЗАЦИЯ объектов,
+    , _rows_num(0)      // то есть вызов конструкторов соответствующих типов
+    , _cols_num{0}      // Фигурные скобки тоже можно (ФСТМ)
+  {
+    // В следующих 3 строчках мы ПЕРЕЗАПИСЫВАЕМ ранее инициализированные объекты,
+    // поэтому использовать это место для инициализации слишком поздно.
+    // Используется оператор присваивания
+    // _storage = nullptr;
+    // _rows_num = 0;
+    // _cols_num = 0;
+  }
+
+  // Инициализирует матрицу m*n.
+  Matrix(size_t rows_num, size_t cols_num);
 private:    
   double* _storage;
-  size_t row;
+  size_t _rows_num;
+  size_t _cols_num;
 };
 
 #endif
